84dc58f46643eaef9b956d8e585232ab603e5c50
Orlando <redbuddylm@gmail.com>
2026-05-06 22:10:29 -0700

    init facturas swing front

---
+++ src/main/java/com/orlando/facturas/Facturas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.orlando.facturas;
+
+/**
+ *
+ * @author Orlando
+ */
+public class Facturas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/orlando/facturas/ui/dialogs/BuscarArticuloDialog.java
@@ -0,0 +1,14 @@
+package com.orlando.facturas.ui.dialogs;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Orlando
+ */
+public class BuscarArticuloDialog {
+    
+}
